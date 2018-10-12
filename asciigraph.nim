b9a7e9143e4e4862ef13a36d6e9336c8c4811bee
Andrew Burns <andy85719@gmail.com>
2018-10-12 09:02:48 -0700

    Minor edits.

--- asciigraph.nim
+++ asciigraph.nim
@@ -3,10 +3,10 @@ import strutils
 import sequtils
 import strfmt
 
-func linearInterpolate*(before, after, atPoint: float64): float64 =
+func linearInterpolate(before, after, atPoint: float64): float64 =
   before + (after - before) * atPoint
 
-func interpolateArray*(data: openArray[float64], fitCount: int): seq[float64] =
+func interpolateArray(data: openArray[float64], fitCount: int): seq[float64] =
   var
     spring: float64
     before: float64
@@ -62,7 +62,7 @@ func plot*(series: openArray[float64], width: int = 0, height: int = 0, offset: 
     width = len(interpolatedSeries) + l_offset
     plot = newSeqWith(rows + 1, newSeq[string](width))
   
-  for i in 0..<rows + 1:
+  for i in 0..rows:
     for j in 0..<width:
       plot[i][j] = " "
   
@@ -110,19 +110,19 @@ func plot*(series: openArray[float64], width: int = 0, height: int = 0, offset: 
     y0 = int(round(interpolatedSeries[x + 0] * ratio) - float64(intmin2))
     y1 = int(round(interpolatedSeries[x + 1] * ratio) - float64(intmin2))
     if y0 == y1:
-      plot[rows-y0][x + l_offset] = "─"
+      plot[rows - y0][x + l_offset] = "─"
     else:
       if y0 > y1:
-        plot[rows-y1][x + l_offset] = "╰"
-        plot[rows-y0][x + l_offset] = "╮"
+        plot[rows - y1][x + l_offset] = "╰"
+        plot[rows - y0][x + l_offset] = "╮"
       else:
-        plot[rows-y1][x + l_offset] = "╭"
-        plot[rows-y0][x + l_offset] = "╯"
+        plot[rows - y1][x + l_offset] = "╭"
+        plot[rows - y0][x + l_offset] = "╯"
     
       var start = int(min(y0, y1)) + 1
       var `end` = int(max(y0, y1))
       for y in start..<`end`:
-        plot[rows-y][x + l_offset] = "│"
+        plot[rows - y][x + l_offset] = "│"
 
   var lines: string
   for h, horizontal in plot:
@@ -138,30 +138,5 @@ func plot*(series: openArray[float64], width: int = 0, height: int = 0, offset: 
   lines
 
 when isMainModule:
-  var 
-    dataSet1 = @[1f64, 1, 1, 1, 1]
-    dataSet2 = @[2f64, 1, 1, 2, -2, 5, 7, 11, 3, 7, 1]
-    dataSet3 = @[2f64, 1, 1, 2, -2, 5, 7, 11, 3, 7, 4, 5, 6, 9, 4, 0, 6, 1, 5, 3, 6, 2]
-    dataSet4 = @[0.2f64, 0.1, 0.2, 2, -0.9, 0.7, 0.91, 0.3, 0.7, 0.4, 0.5]
-    dataSet5 = @[2f64, 1, 1, 2, -2, 5, 7, 11, 3, 7, 1]
-    dataSet6 = @[0.453f64, 0.141, 0.951, 0.251, 0.223, 0.581, 0.771, 0.191, 0.393, 0.617, 0.478]
-    dataSet7 = @[0.01f64, 0.004, 0.003, 0.0042, 0.0083, 0.0033, 0.0079]
-    dataSet8 = @[192f64, 431, 112, 449, -122, 375, 782, 123, 911, 1711, 172]
-    dataSet9 = @[0.3189989805f64, 0.149949026, 0.30142492354, 0.195129182935, 0.3142492354, 0.1674974513, 0.3142492354, 0.1474974513, 0.3047974513]
-    dataSet10 = @[0f64, 0, 0, 0, 1.5, 0, 0, -0.5, 9, -3, 0, 0, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1.5, 0, 0, -0.5, 8, -3, 0, 0, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1.5, 0, 0, -0.5, 10, -3, 0, 0, 1, 2, 1, 0, 0, 0, 0]
-    dataSet11 = @[-5f64, -2, -3, -4, 0, -5, -6, -7, -8, 0, -9, -3, -5, -2, -9, -3, -1]
-    dataSet12 = @[-0.000018527f64, -0.021, -0.00123, 0.00000021312, -0.0434321234, -0.032413241234, 0.0000234234]
-    dataSet13 = @[ 57.76f64, 54.04, 56.31, 57.02, 59.5, 52.63, 52.97, 56.44, 56.75, 52.96, 55.54, 55.09, 58.22, 56.85, 60.61, 59.62, 59.73, 59.93, 56.3, 54.69, 55.32, 54.03, 50.98, 50.48, 54.55, 47.49, 55.3, 46.74, 46, 45.8, 49.6, 48.83, 47.64, 46.61, 54.72, 42.77, 50.3, 42.79, 41.84, 44.19, 43.36, 45.62, 45.09, 44.95, 50.36, 47.21, 47.77, 52.04, 47.46, 44.19, 47.22, 45.55, 40.65, 39.64, 37.26, 40.71, 42.15, 36.45, 39.14, 36.62]
-  echo plot(dataSet1), "\n"
-  echo plot(dataSet2), "\n"
-  echo plot(dataSet3, caption="Plot using asciigraph."), "\n"
-  echo plot(dataSet4, caption="Plot using asciigraph."), "\n"
-  echo plot(dataSet5, height=4, offset=3), "\n"
-  echo plot(dataSet6), "\n"
-  echo plot(dataSet7), "\n"
-  echo plot(dataSet8, height=10), "\n"
-  echo plot(dataSet9, width=30, height=5, caption="Plot with custom height & width."), "\n"
-  echo plot(dataSet10, offset=10, height=10, caption="I'm a doctor, not an engineer."), "\n"
-  echo plot(dataSet11), "\n"
-  echo plot(dataSet12, height=5, width=45), "\n"
-  echo plot(dataSet13)
+  var data = @[3f64, 4, 9, 6, 2, 4, 5, 8, 5, 10, 2, 7, 2, 5, 6]
+  echo plot(data, caption="An example graph!")
